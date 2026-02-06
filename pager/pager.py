446137f02e65bb01a28a0271333f69f0cd450b7b
Brandon Wooding <bw725@ic.ac.uk>
2026-02-06 14:08:03 +0000

    udpated pager to better handle env url

--- pager/pager.py
+++ pager/pager.py
@@ -9,6 +9,9 @@ PAGER_ADDRESS = os.environ.get("PAGER_ADDRESS")
 def page_hospital(mrn, time, pager_url=PAGER_ADDRESS):
     payload = f"{mrn},{time}"
 
+    if not pager_url.startswith(('http://', 'https://')):
+        pager_url = f"http://{pager_url}"
+
     r = requests.post(
         f"{pager_url.rstrip('/')}/page",
         data=payload, 
